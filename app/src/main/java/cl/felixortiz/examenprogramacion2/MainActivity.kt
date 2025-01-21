35a666d1a25eec1f90d3006bb03ddecf37cc4552
Krennic05 <144287905+Krennic05@users.noreply.github.com>
2025-01-20 21:51:53 -0300

    casi listo

--- app/src/main/java/cl/felixortiz/examenprogramacion2/MainActivity.kt
+++ app/src/main/java/cl/felixortiz/examenprogramacion2/MainActivity.kt
@@ -6,8 +6,6 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,7 +16,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
@@ -26,11 +23,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -43,7 +37,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
@@ -51,18 +44,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -71,12 +60,9 @@ import cl.felixortiz.examenprogramacion2.domain.FormatDateUseCase
 import cl.felixortiz.examenprogramacion2.entities.Medidor
 import cl.felixortiz.examenprogramacion2.entities.MedidorRepository
 import cl.felixortiz.examenprogramacion2.entities.TipoMedidor
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.launch
 import java.time.LocalDate
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.util.Date
 
 class MainActivity : ComponentActivity() {
 
@@ -167,8 +153,6 @@ fun ListaMedidoresUI(
                         )
                     )
                     Spacer(modifier = Modifier.width(10.dp))
-                    Log.d("TAG",it.tipo)
-                    Log.d("TAG",TipoMedidor.LUZ.toString())
                     IconoMedidor(it)
                     Spacer(modifier = Modifier.width(10.dp))
                     Column() {
@@ -256,7 +240,8 @@ fun PageFormularioUI(
     { innerPadding ->
     Column(
         modifier = Modifier
-            .fillMaxSize().padding(horizontal = innerPadding.calculateLeftPadding(LayoutDirection.Ltr))
+            .fillMaxSize()
+            .padding(horizontal = innerPadding.calculateLeftPadding(LayoutDirection.Ltr))
             .padding(16.dp),
         verticalArrangement = Arrangement.Center
     ) {
@@ -326,17 +311,19 @@ fun IconoMedidor(medidor: Medidor) {
     Log.d("TAG",medidor.tipo.toUpperCase())
     Log.d("TAG",medidor.tipo)
     Log.d("TAG",TipoMedidor.LUZ.toString())
+    Log.d("TAG",TipoMedidor.GAS.toString())
+    Log.d("TAG",TipoMedidor.AGUA.toString())
     return when(TipoMedidor.valueOf(medidor.tipo.toUpperCase())) {
         TipoMedidor.LUZ -> Image(
-            painter = painterResource(id = R.drawable.luz),
+            painter = painterResource(id = R.drawable._luz),
             TipoMedidor.LUZ.toString(),
         )
         TipoMedidor.GAS -> Icon(
-            painter = painterResource(id = R.drawable.gas),
+            painter = painterResource(id = R.drawable._gas),
             TipoMedidor.GAS.toString(),
         )
         TipoMedidor.AGUA -> Icon(
-            painter = painterResource(id = R.drawable.agua),
+            painter = painterResource(id = R.drawable._agua),
             TipoMedidor.AGUA.toString(),
         )
     }
